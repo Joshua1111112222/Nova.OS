59c4521e27f2583587dee1db7768b4cd25267cf8
Joshua1111112222 <joshuathe2011@gmail.com>
2025-06-29 19:06:25 -0400

    how did I sell the clip for perceptra

--- apps/perceptra-app/app.js
+++ apps/perceptra-app/app.js
@@ -5,5 +5,5 @@ export const app = _component("perceptra-app", html`
     <img src="./apps/perceptra-app/icon.png" alt="Perceptra Icon" style="width:24px; height:24px; vertical-align:middle; margin-right:8px;">
     <span style="font-weight:bold; font-size:14px; vertical-align:middle;">Perceptra</span>
   </div>
-  <iframe src="https://your-pwa-link.com" style="width:100%; height:calc(100% - 32px); border:none;"></iframe>
+  <iframe src="https://joshua1111112222.github.io/Perceptra" style="width:100%; height:calc(100% - 32px); border:none;"></iframe>
 `);
